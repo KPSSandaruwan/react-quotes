8126b2ade9a044d231c29d8fcab2c3912cf40a5b
KPSSandaruwan <kpsurajasandaruwn@gmail.comm>
2022-01-14 12:45:24 +0530

    delete dummy data

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -5,28 +5,6 @@ import useHttp from "../hooks/use-http";
 import { getAllQuotes } from "../lib/api";
 import NoQuotesFound from "../components/quotes/NoQuotesFound"
 
-const DUMMY_QUOTES = [
-  {
-    id: "q1",
-    author: "Nelson Mandela",
-    text: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
-  },
-  {
-    id: "q2",
-    author: "Walt Disney",
-    text: "The way to get started is to quit talking and begin doing.",
-  },
-  {
-    id: "q3",
-    author: "Steve Jobs",
-    text: "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking.",
-  },
-  {
-    id: "q4",
-    author: "Eleanor Roosevelt",
-    text: "If life were predictable it would cease to be life, and be without flavor.",
-  },
-];
 
 const AllQuotes = () => {
   const {
